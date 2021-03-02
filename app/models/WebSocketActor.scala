7ebc1b2de96ea6777be50649ec5e3c33b9964ac1
takatejr <perkadamian@gmail.com>
2021-03-02 18:57:13 +0100

    fix

--- app/models/WebSocketActor.scala
+++ app/models/WebSocketActor.scala
@@ -6,26 +6,22 @@ import play.api.libs.json.Json
 import play.api.libs.streams.ActorFlow
 
 object WebSocketActor {
-
   case class LogHandler(ClientMsg: JsValue)
-  def props(clientActorRef: ActorRef) = Props(
-    new WebSocketActor(clientActorRef)
-  )
+  def props(clientActorRef: ActorRef) = Props(new WebSocketActor(clientActorRef))
   val isActorCreated = false
 }
 
-class WebSocketActor(clientActorRef: ActorRef) extends Actor {
+class WebSocketActor(clientActorRef: ActorRef)(implicit system: ActorSystem) extends Actor {
   import WebSocketActor._
   import LogHandlerActor._
 
   val logger = play.api.Logger(getClass)
+  var cd = true
 
   logger.info(s"WebSocketActor class started")
 
-  override def receive = handleLogger(isActorCreated)
-
-  def handleLogger(isActorCreated: Boolean): Receive = {
-    case LogHandler(jsVal) => {
+  override def receive: Receive = {
+    case jsVal: JsValue => {
       val clientMessage = getMessage(jsVal)
       val UserID = getUser(jsVal)
 
@@ -34,25 +30,25 @@ class WebSocketActor(clientActorRef: ActorRef) extends Actor {
 
       val json: JsValue =
         Json.parse(s"""{"body": "You said, ‘$clientMessage’"}""")
-
-      if (isActorCreated < true) {
+      
+      if (cd.==(true)) {
         val childRef = context.actorOf(
           LogHandlerActor.props(s"$UserID.txt", clientMessage),
           "LogHandler"
         )
-
-        context.become(handleSendToLogger(childRef))
-      }
+        cd = false
+        childRef ! writeToLog(s"$UserID.txt", clientMessage)
+      } 
 
       // clientActorRef ! (json)
-        clientActorRef ! writeToLog(s"$UserID.txt", clientMessage)
+        clientActorRef ! (json)
+
     }
   }
 
   def getMessage(json: JsValue): String = { (json \ "message").as[String] }
 
   def getUser(json: JsValue): Int = { (json \ "UserID").as[Int] }
-
 
   def handleSendToLogger(childRef: ActorRef): Receive = {
     case writeToLog(filePath, message) => childRef forward writeToLog(filePath, message)
